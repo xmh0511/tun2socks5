2689362d567f3063587fb1428ad5b652d3a890f4
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-12-30 17:47:06 +0800

    adjust project structure

--- scripts/rperf.sh
+++ scripts/rperf.sh
@@ -30,8 +30,8 @@ ip netns exec "$netns" ip link set dev lo up
 echo "Starting Dante in background ..."
 ip netns exec "$netns" "$dante" -f ${SCRIPT_DIR}/dante.conf &
 
-# Start iperf3 server in netns
-ip netns exec "$netns" iperf3 -s -B 10.0.0.4 &
+# Start rperf server in netns
+ip netns exec "$netns" rperf -s -B 10.0.0.4 &
 
 sleep 1
 
@@ -44,11 +44,20 @@ ip route add 10.0.0.4 dev tun0
 sleep 3
 
 # Run iperf client through tun2socks5
-iperf3 -c 10.0.0.4 -P 10 -R
+rperf -c 10.0.0.4 -v off -P 1 -r
 
 sleep 3
 
-iperf3 -c 10.0.0.4 -P 10
+rperf -c 10.0.0.4 -v off -P 1
+
+sleep 3
+
+rperf -c 10.0.0.4 -v off -P 1 -u
+
+sleep 3
+
+rperf -c 10.0.0.4 -v trace -P 1 -u -r
+
 
 # Clean up
-# sudo sh -c "pkill tun2socks5; pkill iperf3; pkill danted; ip link del tun0; ip netns del test"
+# sudo sh -c "pkill tun2socks5; pkill rperf; pkill danted; ip link del tun0; ip netns del test"
